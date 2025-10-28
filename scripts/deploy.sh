a71883ed89de6fa87e0289f1c220ecb457d526c1
Rave <rave@utils.studio>
2025-10-28 11:54:53 +0200

    Fix registry name to crontopus-registry

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # Configuration
-REGISTRY="registry.digitalocean.com/crontopus"
+REGISTRY="registry.digitalocean.com/crontopus-registry"
 VERSION="${1:-$(date +%Y%m%d-%H%M%S)}"
 APP_ID="${DIGITALOCEAN_APP_ID:-}"
 
